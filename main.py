4c5dcbb8412a808acd765211f31af5e3a76598d8
felix34ever1 <felix360onea@gmail.com>
2023-02-15 20:48:23 +0000

    Player class
    Added player class and added a turning feature. - Not complete :)

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from player import Player
 
 W_WIDTH = 800
 W_HEIGHT = 600
@@ -9,6 +10,8 @@ race_font = pygame.font.Font("fonts/BebasNeue-Regular.ttf",32)
 
 DISPLAY = pygame.display.set_mode((W_WIDTH,W_HEIGHT))
 pygame.display.set_caption("Racing Game!")
+
+player = Player(DISPLAY,6,1,"images/f1car.png",(30,25))
 
 
 
@@ -21,8 +24,14 @@ while is_running:
         if event.type == pygame.MOUSEBUTTONDOWN:
             has_fastest_time = True
             fastest_time = pygame.time.get_ticks()/1000
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_a:
+                player.turn(True)
+            elif event.key == pygame.K_d:
+                player.turn(False)
     DISPLAY.fill((25,255,25))
 
+    player.update()
 
     time = race_font.render(f"Time:{pygame.time.get_ticks()/1000}",False,(255,255,255),None)
     DISPLAY.blit(time,(0,50))
--- player.py
+++ player.py
@@ -1,3 +1,31 @@
 # Player class - single instanced
 # 
 # Will be a controllable car!
+
+import pygame
+
+
+class Player():
+
+    def __init__(self,DISPLAY: pygame.surface.Surface,speed,acceleration,image: str,starting_pos) -> None:
+        self.speed = speed
+        self.acceleration = acceleration
+        self.original_image = pygame.image.load(image)
+        self.image = self.original_image
+        self.DISPLAY = DISPLAY
+        self.angle = 0
+
+        self.rect = self.image.get_rect()
+        self.rect.topleft = ((starting_pos[0],starting_pos[1]))
+    
+    def update(self):
+        self.rect = self.image.get_rect()
+        self.DISPLAY.blit(self.image,self.rect)
+
+    def turn(self,left:bool):
+        if left:
+            self.image = pygame.transform.rotate(self.original_image,self.angle+1)
+            self.angle+=1
+        else:
+            self.image = pygame.transform.rotate(self.original_image,self.angle-1)
+            self.angle-=1
